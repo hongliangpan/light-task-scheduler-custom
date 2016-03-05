4cf38a2a739f02ebf7c39e4bdb973ed68c257e3b
hongliangpan <28797575@qq.com>
2016-03-05 14:34:59 +0800

    doc

---
+++ lts-app-custom/src/main/java/com/lts/startup/TaskTrackerStartupCustom.java
@@ -0,0 +1,48 @@
+package com.lts.startup;
+
+import com.lts.tasktracker.TaskTracker;
+
+import java.util.Map;
+
+/**
+ * @author Robert HG (254963746@qq.com) on 9/11/15.
+ */
+public class TaskTrackerStartupCustom {
+
+    public static void main(String[] args) {
+        String cfgPath = args[0];
+        start(cfgPath);
+    }
+
+    public static void start(String cfgPath) {
+        try {
+            TaskTrackerCfg cfg = TaskTrackerCfgLoader.load(cfgPath);
+
+            final TaskTracker taskTracker;
+
+            if (cfg.isUseSpring()) {
+                taskTracker = SpringStartup.start(cfg, cfgPath);
+            } else {
+                taskTracker = DefaultStartup.start(cfg);
+            }
+            //启动参数
+            for (Map.Entry<String, String> config : cfg.getConfigs().entrySet()) {
+                taskTracker.addConfig(config.getKey(), config.getValue());
+            }
+
+            taskTracker.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    taskTracker.stop();
+                }
+            }));
+
+        } catch (CfgException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
